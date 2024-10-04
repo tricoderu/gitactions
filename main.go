f3262c843f5983d1ed910a16300839ac0000552f
Евгений Карпеев <e.karpeev@gmail.com>
2024-10-04 08:49:14 +0300

    Racetest

--- main.go
+++ main.go
@@ -1,8 +1,38 @@
 // main.go
-package main
+// БЕЗ ГОНКИ
+/*package main
 
 func main() {
 
+}
+
+func MaxInt(a, b int) int {
+	if a >= b {
+		return a
+	}
+
+	return b
+}*/
+// С ГОНКОЙ
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(5)
+
+	for i := 0; i < 5; i++ {
+		go func() {
+			fmt.Println(i)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
 }
 
 func MaxInt(a, b int) int {
